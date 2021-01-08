85defcd8ba8c13c1d071dec7d4e40ed539f273a5
Toasty-Laptop <curtisgauntdev@gmail.com>
2021-01-08 15:49:09 -0500

    fixed saving of variables producing garbage characters

--- GuildBankAudit.lua
+++ GuildBankAudit.lua
@@ -22,6 +22,7 @@ end
 function printHelp()
   print("----- |cff26c426Guild Bank Audit Options|r -----")
   print("Type the slash command followed by one of the options below -> '/gba command'")
+  print("|cffc21e1eDATA WILL NOT BE SAVED UNTIL YOU LOG OUT OF YOUR CHARACTER!|r")
   print("|cff5fe65dall|r", " - Scans your entire guild bank.")
   print("|cff5fe65dtab|r", " - Scans the current tab open in your guild bank.")
   print("|cff5fe65dhelp|r", " - Displays this information here.")
@@ -40,11 +41,13 @@ function scanBank()
       local itemTex, itemCount, itemLocked, itemFiltered, itemQuality = GetGuildBankItemInfo(i, k)
       local itemName = GetGuildBankItemLink(i, k)
       if itemName ~= nil then
-        tinsert(SavedBankItemInfo, format("%s, %c,", itemName, tostring(itemCount)))
+      --  tinsert(SavedBankItemInfo, format("%s, %c,", itemName, tostring(itemCount)))
+        tinsert(SavedBankItemInfo, itemName)
+        tinsert(SavedBankItemInfo, itemCount)
       end
     end
   end
-  print("|cff26c426Guild Bank Audit Complete!|r")
+  print("|cff26c426Guild Bank Audit Complete!|r Data will be saved when you log out.")
 end
 
 function scanTab()
@@ -56,8 +59,10 @@ function scanTab()
     local itemTex, itemCount, itemLocked, itemFiltered, itemQuality = GetGuildBankItemInfo(currentTab, i)
     local itemName = GetGuildBankItemLink(currentTab, i)
     if itemName ~= nil then
-      tinsert(SavedBankItemInfo, format("%s, %c,", itemName, tostring(itemCount)))
+    --  tinsert(SavedBankItemInfo, format("%s, %c,", itemName, tostring(itemCount)))
+      tinsert(SavedBankItemInfo, itemName)
+      tinsert(SavedBankItemInfo, itemCount)
     end
   end
-  print("|cff26c426Guild Bank Tab Audit Complete!|r")
+  print("|cff26c426Guild Bank Tab Audit Complete!|r Data will be saved when you log out.")
 end
